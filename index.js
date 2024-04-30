410dd146d17b0bf948b7b7900131699ef6cffacf
michael <michael@eeproto.com>
2024-04-29 23:49:16 -0400

    add id properties to directions and ingredients. Add get recipes and recipe routes. add post recipe route. add recipe update route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import { PORT } from './config.js';
+import { Recipe } from './models/recipeModel.js';
 
 dotenv.config();
 
@@ -10,9 +11,58 @@ const mongoDbUrl = process.env.MONGO_DB_URL;
 
 const app = express();
 
-app.get('/', (request, response) => {
-  console.log(request);
-  return response.status(234).send('First route');
+app.use(express.json());
+
+app.get('/recipes', async (request, response) => {
+  try {
+    const recipes = await Recipe.find({});
+    return response.status(200).send({ count: recipes.length, data: recipes });
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+app.get('/recipes/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const recipe = await Recipe.findById(id);
+    return response.status(200).send(recipe);
+  } catch (error) {
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+app.post('/recipes', async (request, response) => {
+  try {
+    const newRecipe = request.body;
+    if (!newRecipe.title) {
+      return response
+        .status(400)
+        .send({ message: 'Send all required fields: title' });
+    }
+    const recipe = await Recipe.create(newRecipe);
+    return response.status(201).send(recipe);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.put('/recipes/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Recipe.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).send({ message: 'Recipe not found'});
+    }
+
+    return response.status(200).send({ message: 'Recipe update successfully'});
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message});
+  }
 });
 
 mongoose
--- models/recipeModel.js
+++ models/recipeModel.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
 const directionSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
   directionText: {
     type: String,
     required: true,
@@ -12,6 +16,10 @@ const directionSchema = new mongoose.Schema({
 });
 
 const ingredientSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
@@ -27,39 +35,42 @@ const ingredientSchema = new mongoose.Schema({
   note: {
     type: String,
     required: false,
-  }
+  },
 });
 
-const recipeSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: false,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    servings: {
+      type: Number,
+      required: false,
+    },
+    caloriesPerServing: {
+      type: Number,
+      required: false,
+    },
+    totalMinutes: {
+      type: Number,
+      required: false,
+    },
+    rating: {
+      type: Number,
+      required: false,
+    },
+    ingredients: [ingredientSchema],
+    directions: [directionSchema],
   },
-  imageUrl: {
-    type: String,
-    required: false,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  servings: {
-    type: Number,
-    required: false,
-  },
-  caloriesPerServing: {
-    type: Number,
-    required: false,
-  },
-  totalMinutes: {
-    type: Number,
-    required: false,
-  },
-  rating: {
-    type: Number,
-    required: false,
-  },
-  ingredients: [ingredientSchema],
-  directions: [directionSchema],
-});
-export const recipe = mongoose.model('Recipe', recipeSchema);
+  { timestamps: true }
+);
+export const Recipe = mongoose.model('Recipe', recipeSchema);
